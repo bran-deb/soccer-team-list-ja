234553e21f4318bbaccc4003c052f93c07a35aa2
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-10-06 00:03:42 -0400

    Create db connection

---
+++ src/main/java/org/bpadilla/java/jdbc/EjemploJdbc.java
@@ -0,0 +1,26 @@
+package org.bpadilla.java.jdbc;
+
+import java.sql.*;
+
+public class EjemploJdbc {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC";
+        String username = "root";
+        String password = "sasa";
+        try {
+            Connection conn = DriverManager.getConnection(url,username,password);
+            Statement stmt = conn.createStatement();
+            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+
+            while (resultado.next()){
+                System.out.println(resultado.getString("nombre"));
+            }
+            resultado.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
